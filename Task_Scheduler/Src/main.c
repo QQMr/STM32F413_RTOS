3f2cc1a01d59fba550d27ef76ebc1664d2918348
QQ <cchangwo@yahoo.com.tw>
2022-05-08 01:26:36 +0800

    Create user tasks

--- Task_Scheduler/Src/main.c
+++ Task_Scheduler/Src/main.c
@@ -16,14 +16,54 @@
  ******************************************************************************
  */
 
+
 #include <stdint.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+void task1_handler(void);
+void task2_handler(void);
+void task3_handler(void);
+void task4_handler(void);
+
+
 int main(void)
 {
     /* Loop forever */
+	printf("Hello World\n");
 	for(;;);
+}
+
+void task1_handler(void)
+{
+	while(1)
+	{
+		printf("This is task1\n");
+	}
+}
+
+void task2_handler(void)
+{
+	while(1)
+	{
+		printf("This is task1\n");
+	}
+}
+
+void task3_handler(void)
+{
+	while(1)
+	{
+		printf("This is task1\n");
+	}
+}
+
+void task4_handler(void)
+{
+	while(1)
+	{
+		printf("This is task1\n");
+	}
 }
